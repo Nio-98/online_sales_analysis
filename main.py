96c911a9ff26e10ad007b20c416542bf28c66534
Danilo Kovac <drvozapbe@gmail.com>
2026-03-11 00:03:58 +0100

    Updated products and removed inventory display

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ from product_manager import ProductManager
 
 manager = ProductManager()
 
-p1 = Product("Laptop", 1000, 5)
+p1 = Product("Laptop Pro", 1200, 3)
 p2 = Product("Mouse", 25, 20)
 p3 = Product("Keyboard", 50, 10)
 
 manager.add_product(p1)
 manager.add_product(p2)
 manager.add_product(p3)
-
-manager.show_products()
-
-print("Total inventory value:", manager.total_value())
