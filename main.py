47b5816d42b64cda606e9d1d3f7bb412f33a480d
Danilo Kovac <drvozapbe@gmail.com>
2026-03-10 23:58:54 +0100

    Initial project with Product and ProductManager classes

---
+++ main.py
@@ -0,0 +1,16 @@
+from product import Product
+from product_manager import ProductManager
+
+manager = ProductManager()
+
+p1 = Product("Laptop", 1000, 5)
+p2 = Product("Mouse", 25, 20)
+p3 = Product("Keyboard", 50, 10)
+
+manager.add_product(p1)
+manager.add_product(p2)
+manager.add_product(p3)
+
+manager.show_products()
+
+print("Total inventory value:", manager.total_value())
